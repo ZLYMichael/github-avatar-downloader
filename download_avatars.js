381053fb4efddbf17b2623d4f95fd3e98c33e364
vagrant <zlymike@outlook.com>
2017-06-05 23:59:06 +0000

    step 4 done

--- download_avatars.js
+++ download_avatars.js
@@ -2,8 +2,14 @@ var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+var GITHUB_USER = "ZLYMichael";
+var GITHUB_TOKEN ="1fe7d58e91f4042d4414d6fe094f87c020177178";
+
 function getRepoContributors(repoOwner, repoName, cb) {
+  var requestURL ='https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName +'/contributors';
   request.get('https://api.github.com/repos/jquery/jquery/contributors')
+  console.log(requestURL);
+
 }
 
 
@@ -11,3 +17,4 @@ getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("errors:", err);
   console.log("Result:", result)
 });
+
