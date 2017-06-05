135ceb1a75a38054ed1d5c7f0edf661f8ab2bedf
vagrant <zlymike@outlook.com>
2017-06-05 23:50:42 +0000

    corrected

--- download_avatars.js
+++ download_avatars.js
@@ -2,10 +2,12 @@ var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-
-
-
 function getRepoContributors(repoOwner, repoName, cb) {
+  request.get('https://api.github.com/repos/jquery/jquery/contributors')
+}
+
+
+getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("errors:", err);
   console.log("Result:", result)
-}
+});
