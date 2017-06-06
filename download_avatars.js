be5717aa298c32cd00b0bb4bfb442dae81e29751
vagrant <zlymike@outlook.com>
2017-06-06 02:31:21 +0000

    finished part 7 and 8

--- download_avatars.js
+++ download_avatars.js
@@ -4,10 +4,10 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 
 var GITHUB_USER = "ZLYMichael";
 var GITHUB_TOKEN ="1fe7d58e91f4042d4414d6fe094f87c020177178";
+var fs =require('fs');
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var requestURL ='https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName +'/contributors';
-
   var options = {
     url: requestURL,
     headers: {
@@ -30,9 +30,13 @@ getRepoContributors("jquery", "jquery", function(err, contributors) {
   if (err) {
     console.log("errors:", err);
   } else {
-    for(i in contributors) {
-        console.log(contributors[i].avatar_url);
+    for(i of contributors) {
+        downloadImageByURL(i.avatar_url ,i.login + ".jpg");
       }
     }
 });
 
+function downloadImageByURL(url, fileName) {
+  request.get(url)
+  .pipe(fs.createWriteStream('./avatars/' + fileName))
+}
