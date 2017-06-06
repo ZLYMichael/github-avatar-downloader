82a2f0b0e24326019f168961de25cb086677ec55
vagrant <zlymike@outlook.com>
2017-06-06 03:32:34 +0000

    part 9 done

--- download_avatars.js
+++ download_avatars.js
@@ -4,10 +4,13 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 
 var GITHUB_USER = "ZLYMichael";
 var GITHUB_TOKEN ="1fe7d58e91f4042d4414d6fe094f87c020177178";
-var fs =require('fs');
+var fs = require('fs');
+// var owner = process.argv.slice(2)[0].toLowerCase();
+var owner = process.argv[2];
+var repo = process.argv[3]
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  var requestURL ='https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName +'/contributors';
+  var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName +'/contributors';
   var options = {
     url: requestURL,
     headers: {
@@ -25,18 +28,17 @@ function getRepoContributors(repoOwner, repoName, cb) {
   });
 }
 
-
-getRepoContributors("jquery", "jquery", function(err, contributors) {
-  if (err) {
-    console.log("errors:", err);
-  } else {
-    for(i of contributors) {
-        downloadImageByURL(i.avatar_url ,i.login + ".jpg");
-      }
-    }
-});
-
 function downloadImageByURL(url, fileName) {
   request.get(url)
   .pipe(fs.createWriteStream('./avatars/' + fileName))
 }
+
+getRepoContributors(owner, repo, function(err, contributors) {
+  if (err) {
+    console.log("errors:", err);
+  } else {
+    for(var i of contributors) {
+      downloadImageByURL(i.avatar_url, i.login + ".jpg");
+    }
+  }
+});
